85152e27edcadec12a347500e50156da0d8b005f
Sreekar33 <cs14btech11033@iith.ac.in>
2022-11-13 04:44:08 -0600

    corrections in addition

--- src/app/resume-builder-pg3/resume-builder-pg3.component.ts
+++ src/app/resume-builder-pg3/resume-builder-pg3.component.ts
@@ -15,13 +15,24 @@ export class ResumeBuilderPg3Component implements OnInit {
   accounts: Account[] = [];
   path = "/resume";
   logAccount: Account = {firstN:"", lastN:"", email:"", password:""};
-
+  schoolname : string = "";
+  schoolcity : string = "";
+  schoolstate : string = "";
+  degree : string = "";
   constructor(private usersListService : UsersListService){
     this.accounts = this.usersListService.accounts
   }
 
   ngOnInit(): void {
     this.currentAccount = this.usersListService.currentAccount;
+    if(localStorage.getItem('pg3Data')){
+      console.log(localStorage.getItem('pg1Data'));
+      let obj = JSON.parse(localStorage.getItem('pg3Data')|| '{}');
+      this.schoolname = obj.schoolname;
+      this.schoolcity = obj.schoolcity;
+      this.schoolstate = obj.schoolstate;
+      this.degree = obj.degree;
+    }
     console.log("Sreekar"+this.accounts)
     console.log(this.accounts)
     // console.log(UsersListService.currentAccount);
@@ -37,6 +48,13 @@ export class ResumeBuilderPg3Component implements OnInit {
     this.appliedJobsClicked = true;
     this.suggestedJobsClicked = false;
   }
-
+  readData(value: string):void{
+    var request: any = {};
+    request.schoolname = (<HTMLInputElement>document.getElementById("schoolname")).value;
+    request.schoolcity = (<HTMLInputElement>document.getElementById("schoolcity")).value;
+    request.schoolstate = (<HTMLInputElement>document.getElementById("schoolstate")).value;
+    request.degree = (<HTMLInputElement>document.getElementById("degree")).value;
+    localStorage.setItem('pg3Data', JSON.stringify(request));
+  }
 
 }
