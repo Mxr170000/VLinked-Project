701f5681c0358b8ea4c96d72b940cfd59140668e
Sreekar33 <cs14btech11033@iith.ac.in>
2022-11-14 10:58:57 -0600

    Auto-generated commit [ci skip]

---
+++ assets/utilities.ts
@@ -0,0 +1,28 @@
+export const styles = {
+  head: {
+    font: "Aleo",
+    bold: true,
+    color: "#000",
+    fontSize: 32,
+  },
+};
+
+export const defaultStyle = {
+  font: "Assistant",
+  bold: false,
+  color: "#000",
+  fontSize: 10,
+};
+export const fonts = {
+  Aleo: {
+    bold: "Aleo-Bold.ttf",
+  },
+  Assistant: {
+    normal: "Assistant-Regular.ttf",
+    bold: "Assistant-Medium.ttf",
+  },
+  Icomoon: {
+    normal: "icomoon.ttf",
+  },
+};
+
