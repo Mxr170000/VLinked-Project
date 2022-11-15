abcb94e8fa1a392c76f5cc431850dd7e8c941ebc
unknown <lepehina37@gmail.com>
2022-11-14 18:44:33 -0600

    Json file with jobs created

---
+++ job_data.js
@@ -0,0 +1,18 @@
+var jobs =[
+    {
+        "id" : "1",
+        "title" : "Cyber Security Engineer",
+        "location" : "Dallas",
+        "short_description" : "",
+        "long_description" : "",
+        "benefits" : "",
+        "company" : "",
+        "num_veterans_hired" : "",
+        "date" : "",
+        "experience" : "",
+
+
+
+
+    }
+]
