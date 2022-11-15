92827089a0d38c27ad3d4187b680771712a90150
Sreekar33 <cs14btech11033@iith.ac.in>
2022-11-14 22:51:01 -0600

    Improvements added

--- src/app/job-finder-pg1/job-finder-pg1.component.ts
+++ src/app/job-finder-pg1/job-finder-pg1.component.ts
@@ -11,6 +11,9 @@ export class JobFinderPg1Component implements OnInit {
   count:number = 0;
   location: String = "";
   industry: String = "";
+  sorta:Boolean = true;
+  sortb:Boolean = true;
+  sortc:Boolean = true;
   constructor(private activatedRoute: ActivatedRoute) { 
     
   }
@@ -25,8 +28,8 @@ export class JobFinderPg1Component implements OnInit {
       this.appliedJobs = obj.appliedjobs;
     }
     this.activatedRoute.queryParams.subscribe(params => {
-        this.location =  params['location'];
-        this.industry =  params['industry'];
+        this.location =  params['location'] || "";
+        this.industry =  params['industry'] || "";
       });
     
     this.appendOptions();
@@ -37,7 +40,7 @@ export class JobFinderPg1Component implements OnInit {
     for (var job of jobs){
       if ((this.location=="" || job.location.toLowerCase().replace(/\s/g, '') == this.location) && (this.industry=="" || job.industry.toLowerCase().replace(/\s/g, '') == this.industry)){
         this.count+=1;
-        var text = "<div class='card mt-3 to-display-func' value='"+job.id+"' style='width: 18rem;'><div class='card-body mt-4 mr-1'><h5 class='card-title'>"+job.title+"</h5><p class='card-text'>at "+job.company +"</p><p class='card-text'>"+job.location+"</p><p class='card-text'>"+job.short_description+"</p>"
+        var text = "<div class='card mt-3 to-display-func' value='"+job.id+"' style='width: 18rem;'><div class='card-body mt-4 mr-1'><h5 class='card-title'>"+job.title+"</h5><p class='card-text'>at "+job.company +"</p><p class='card-text'>"+job.location+"</p><p class='card-text'>Date Posted: "+job.date+"</p><p class='card-text'> Number of Veterans Hired: "+job.num_veterans_hired+"</p><p class='card-text'>Experience Required(in years): "+job.experience+"</p><p class='card-text'>"+job.short_description+"</p>"
         if (this.appliedJobs.includes(job.id)){
             text += "<button type='button' class='btn btn-secondary'>Applied</button>"
         }
@@ -120,19 +123,37 @@ export class JobFinderPg1Component implements OnInit {
   }
 
   sortByDate():void{
-    jobs.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+    if (this.sorta==true){
+      jobs.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+      this.sorta = false;
+    }
+    else{
+      jobs.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+      this.sorta = true; 
+    }
     this.appendOptions();
   }
-
   sortByNumber():void{
-
-    jobs.sort((a, b) => parseInt(b.num_veterans_hired) - parseInt(a.num_veterans_hired));
+    if (this.sortb==true){
+      jobs.sort((a, b) => Number(b.num_veterans_hired) - Number(a.num_veterans_hired));
+      this.sortb = false;
+    }
+    else{
+      jobs.sort((a, b) => Number(a.num_veterans_hired) - Number(b.num_veterans_hired));
+      this.sortb = true; 
+    }
     this.appendOptions();
     
   }
-
   sortByExperience():void{
-    jobs.sort((a, b) => parseInt(a.experience) - parseInt(b.experience));
+    if (this.sortc==true){
+      jobs.sort((a, b) => Number(b.experience) - Number(a.experience));
+      this.sortc = false;
+    }
+    else{
+      jobs.sort((a, b) => Number(a.experience) - Number(b.experience));
+      this.sortc = true; 
+    }
     this.appendOptions();
     
   }
