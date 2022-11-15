8c1d990639445f00d3bf45db7e9686383a79717a
Sreekar33 <cs14btech11033@iith.ac.in>
2022-11-14 23:25:41 -0600

    fixes in the home page

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Account } from '../account';
 import { UsersListService } from '../users-list.service';
-
+import {jobs} from '../job-finder-pg1/job_data'
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
@@ -12,23 +12,40 @@ export class HomeScreenComponent implements OnInit {
   currentAccount: Account = {firstN:"", lastN:"", email:"", password:""};
   suggestedJobsClicked: Boolean = true;
   appliedJobsClicked: Boolean = false;
-
+  jobsapplied:any;
   constructor(private usersListService : UsersListService) { }
 
   ngOnInit(): void {
     this.currentAccount = this.usersListService.currentAccount;
-    console.log(this.usersListService.currentAccount);
-    console.log(this.usersListService.accounts);
+    if(localStorage.getItem('appliedJobs')){
+      console.log(localStorage.getItem('appliedJobs'));
+      let obj = JSON.parse(localStorage.getItem('appliedJobs')|| '{}');
+      this.jobsapplied = obj.appliedjobs;
+    }
+    
+    
   }
 
   suggestedJobs():void{
     this.suggestedJobsClicked = true;
     this.appliedJobsClicked = false;
+    const doc = window.document.getElementById("jobdiv")!
+      doc.innerHTML = "";
   }
 
   appliedJobs():void{
     this.appliedJobsClicked = true;
     this.suggestedJobsClicked = false;
+    //console.log((document.getElementById("jobdiv")).innerHTML);
+    if(this.jobsapplied.length>0){
+      const doc = window.document.getElementById("jobdiv")!
+      doc.innerHTML = "";
+      for (let i = 0; i < jobs.length; i++) {
+        if(this.jobsapplied.includes(jobs[i].id)){
+          doc.innerHTML += "<h2 style='padding-left: 10%'> "+jobs[i].title+" At "+jobs[i].company+", "+jobs[i].location+"</h2>";
+        }
+      }
+    }
   }
 
 }
